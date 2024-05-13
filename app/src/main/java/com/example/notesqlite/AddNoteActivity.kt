85a7da4b75910b5324ccf6f8921dc273510b4d02
Kavindu Galgamuwa <125373634+kavindunilusha@users.noreply.github.com>
2024-05-13 13:05:03 +0530

    add validation

--- app/src/main/java/com/example/notesqlite/AddNoteActivity.kt
+++ app/src/main/java/com/example/notesqlite/AddNoteActivity.kt
@@ -24,12 +24,23 @@ class AddNoteActivity : AppCompatActivity() {
         db = NotesDatabaseHelper(this)
 
         binding.saveButton.setOnClickListener{
-            val title = binding.titleEditText.text.toString()
-            val content = binding.contentEditText.text.toString()
-            val note = Note(0,title,content)
+            val title = binding.titleEditText.text.toString().trim()
+            val content = binding.contentEditText.text.toString().trim()
+
+            if (title.isEmpty()) {
+                binding.titleEditText.error = "Title cannot be empty"
+                return@setOnClickListener
+            }
+
+            if (content.isEmpty()) {
+                binding.contentEditText.error = "Content cannot be empty"
+                return@setOnClickListener
+            }
+
+            val note = Note(0, title, content)
             db.insertNote(note)
             finish()
-            Toast.makeText(this,"Note Saved.",Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "Note Saved.", Toast.LENGTH_SHORT).show()
         }
         }
 
